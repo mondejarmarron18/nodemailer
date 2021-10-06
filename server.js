9c9ff6e31b39087ba43e840c1b90d188b7ef0ac3
mondejarmarron18 <mondejarmarron18@gmail.com>
2021-10-06 20:51:34 +0800

    Add GUI

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 import dotenv from "dotenv";
 import nodemailer from "nodemailer";
 import express from "express";
+import cors from "cors";
 
 const app = express();
 dotenv.config();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 app.post("/", (req, res) => {
-  const email = req.headers.email;
-  const message = req.headers.message;
-
+  const email = req.body.email;
+  const message = req.body.message;
   const transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
@@ -18,18 +19,16 @@ app.post("/", (req, res) => {
       pass: process.env.PASSWORD,
     },
   });
-
   const mailOptions = {
     from: process.env.EMAIL,
     to: email,
     subject: "MarronTech",
     html: `Hello! Thank you for sending a message.<br/><br/>This is an automated responce. <br/><br/> I will make sure that I reply to you as soon as I read your message, I typically reply within 24 hours and just for you to remember, here's' your message to us<br/><br/> <blockquote>"${message}"</blockquote><br/><br/><b>Alrights Reserved &copy; 2021</b><br/><b>www.marrontech.me</b>`,
   };
-
   transporter.sendMail(mailOptions, (err, data) => {
     return err
-      ? res.status(400).send(err)
-      : res.status(200).send(data.response);
+      ? res.status(400).json(err)
+      : res.status(200).json(data.response);
   });
 });
 
